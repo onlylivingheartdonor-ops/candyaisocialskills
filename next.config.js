e522743cb3facd23543c9043d5a3248013147b53
David M. Graham <onlylivingheartdonor.com>
2026-05-28 10:28:20 -0400

    Add navigation link to MoneyWise Calculator

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  trailingSlash: false,
+  
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'candyaisocialskills.com' }],
+        destination: 'https://www.candyaisocialskills.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
